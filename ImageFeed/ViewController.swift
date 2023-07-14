200dfb8a7ce26fe1be7b7ddfcdbe3b54fe077266
Aleksandr Skripnikov <apskripnikov@yandex.ru>
2023-07-14 13:33:48 +0700

    Initial Commit

---
+++ ImageFeed/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageFeed
+//
+//  Created by Александр Скрипников on 14/7/2566 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
