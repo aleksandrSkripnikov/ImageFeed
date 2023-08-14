31644d3431790e093e38e03ede3b3d2141b417b9
aleksandrSkripnikov <apskripnikov@yandex.ru>
2023-08-15 00:18:34 +0700

    В проект внесены изменения созласно заданию по спринту 10.

---
+++ ImageFeed/Constants.swift
@@ -0,0 +1,7 @@
+import Foundation
+
+let AccessKey = "3kuVX7jeU0OqCGiZ6XKrGh4x973NpfAj4EJXQ7zYmX8"
+let SecretKey = "u9nvrspUzy4izuFJ2Kqf0dYQWVCTpkEak88LAclrYFg"
+let RedirectURI = "urn:ietf:wg:oauth:2.0:oob"
+let AccessScope = "public+read_user+write_likes"
+let DefaultBaseURL = URL(string: "https://api.unsplash.com")!
